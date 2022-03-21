746d9c558f60b2c4d12ae4a00c6d7a82222f02ab
Wolfst4r <102064774+Wolfst4r@users.noreply.github.com>
2022-03-21 14:47:15 -0300

    Add files via upload

---
+++ p5play_template-main/sketch.js
@@ -0,0 +1,28 @@
+var caixa;
+
+function setup() {
+  createCanvas(400,400);
+  caixa=createSprite (200,200,30,30);
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites();
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    caixa.x+= 5;
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+    caixa.x-= 5;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    caixa.y-= 5;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    caixa.y+= 5;
+  }
+
+
+}
+
